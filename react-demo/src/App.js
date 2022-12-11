842a930af63be55bfa460b4a716214bcce41e35d
henry <hsuanci@gmail.com>
2022-12-11 12:37:20 +0800

    add / reusable components /w props data input

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,13 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { title: "bookOutOne", amount: 20, date: new Date(2022, 1, 28) },
+    { title: "bookOutTwo", amount: 30, date: new Date(2022, 2, 28) },
+    { title: "bookOutThree", amount: 40, date: new Date(2022, 3, 28) },
+  ];
   return (
     <div className="App">
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+         <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+         <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
     </div>
-    
   );
 }
 
